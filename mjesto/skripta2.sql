6eb1201dd172b568858d1ecf14386285f6c180e1
isidorakaran <karanisidora@hotmail.com>
2022-11-27 22:13:11 +0100

    Create skripta2.sql

---
+++ mjesto/skripta2.sql
@@ -0,0 +1,24 @@
+select * from mjesto;
+
+
+select * from mjesto m 
+where naziv like '%eb%';
+
+select * from mjesto m 
+where zupanija like '%21%';
+
+delete from mjesto 
+where zupanija = 21;
+
+update mjesto 
+set naziv='GORNJI STUPNIK'
+where postanskibroj = 10255;
+
+select * from mjesto m 
+where naziv = 'Osijek';
+
+select * from mjesto m 
+where zupanija = 14;
+
+select * from mjesto m 
+where naziv like '%nt';
