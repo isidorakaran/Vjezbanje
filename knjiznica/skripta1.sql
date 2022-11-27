df7cd4922c6965b86375150519703802e1b344ab
isidorakaran <karanisidora@hotmail.com>
2022-11-27 20:42:35 +0100

    Vjezba

--- knjiznica/skripta1.sql
+++ knjiznica/skripta1.sql
@@ -27,3 +27,22 @@ FROM katalog k
 inner JOIN mjesto m 
 ON k.mjesto = m.sifra 
 where m.drzava  = 'Hrvatska';
+
+select * from autor a 
+where (ime, prezime) = ('Isidora', 'Karan');
+
+insert into autor (sifra,ime,prezime,datumrodenja)
+values (6,'Goran', 'Maras', '1995-03-19');
+
+
+select * from autor;
+
+delete from autor 
+where sifra=0;
+
+delete from autor 
+where sifra in (5,6);
+
+update autor
+set ime = 'Isidora',prezime = 'Karanic'
+where sifra=4;
