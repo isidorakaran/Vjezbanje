9eca8e97864b28ed160d9a91291668939c8c8c71
isidorakaran <karanisidora@hotmail.com>
2022-12-05 20:05:44 +0100

    Dodane tablice

---
+++ kolokvij/skripta4.sql
@@ -0,0 +1,75 @@
+drop database if exists baza4;
+create database baza4;
+use baza4;
+
+create table snasa(
+   sifra int not null primary key auto_increment,
+   introvertno bit,
+   treciputa datetime,
+   haljina varchar(44) not null,
+   zena int not null
+);
+
+create table becar(
+    sifra int not null primary key auto_increment,
+    novcica decimal(14,8),
+    kratkamajica varchar(48) not null,
+    bojaociju varchar(36) not null,
+    snasa int not null
+);
+
+create table prijatelj(
+    sifra int not null primary key auto_increment,
+    eura decimal(16,9),
+    prstena int not null,
+    gustoca decimal(16,5),
+    jmbag char(11) not null,
+    suknja varchar(47) not null,
+    becar int not null
+);
+
+create table zena(
+    sifra int not null primary key auto_increment,
+    suknja varchar(39) not null,
+    lipa decimal(18,7),
+    prstena int not null
+);
+
+create table zena_mladic(
+    sifra int not null primary key auto_increment,
+    zena int not null,
+    mladic int not null
+);
+
+create table mladic(
+    sifra int not null primary key auto_increment,
+    kuna decimal(15,9),
+    lipa decimal(18,5),
+    nausnica int,
+    stilfrizura varchar(49),
+    vesta varchar(34) not null
+);
+
+create table punac(
+    sifra int not null primary key auto_increment,
+    treciputa datetime,
+    majica varchar(46),
+    jmbag char(11) not null,
+    novcica decimal(18,7) not null,
+    maraka decimal(12,6) not null,
+    ostavljen int not null
+);
+
+create table ostavljen(
+    sifra int not null primary key auto_increment,
+    modelnaocala varchar(43),
+    introvertno bit,
+    kuna decimal(14,10)
+);
+
+alter table punac add foreign key (ostavljen) references ostavljen(sifra);
+alter table zena_mladic add foreign key (zena) references zena(sifra);
+alter table zena_mladic add foreign key (mladic) references mladic(sifra);
+alter table snasa add foreign key (zena) references zena(sifra);
+alter table becar add foreign key (snasa) references snasa(sifra);
+alter table prijatelj add foreign key (becar) references becar(sifra);
