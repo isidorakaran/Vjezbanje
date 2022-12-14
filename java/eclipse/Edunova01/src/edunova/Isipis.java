e5d2257cb46d2fa9a44322647967841aa1401b02
isidorakaran <karanisidora@hotmail.com>
2022-12-14 21:03:23 +0100

    Vjezba

---
+++ java/eclipse/Edunova01/src/edunova/Isipis.java
@@ -0,0 +1,11 @@
+package edunova;
+
+public class Isipis {
+	public static void main(String[] args) {
+		System.out.println("Prva linija");
+		System.out.println("Druga linija");
+		System.out.println("Prva linija\nDruga linija\n");
+		System.out.println("\tOsijek");
+	}
+
+}
