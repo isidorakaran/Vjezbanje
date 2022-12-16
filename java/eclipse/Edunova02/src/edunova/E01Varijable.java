33a86dfd92f2cfa17bc67ea934a5919a31541ecb
isidorakaran <karanisidora@hotmail.com>
2022-12-16 20:39:33 +0100

    Vjezba2

--- java/eclipse/Edunova02/src/edunova/E01Varijable.java
+++ java/eclipse/Edunova02/src/edunova/E01Varijable.java
@@ -1,29 +1,28 @@
 package edunova;
 
 public class E01Varijable {
-	
+
 	public static void main(String[] args) {
 		int i;
-		i=7;
+		i = 7;
 		System.out.println(i);
-		
+
 		int j;
-		j=8;
+		j = 8;
 		System.out.println(j);
-		
-		int x,y,z;
-		x=1;
-		y=2;
-		z=3;
+
+		int x, y, z;
+		x = 1;
+		y = 2;
+		z = 3;
 		System.out.println(x);
 		System.out.println(y);
 		System.out.println(z);
-		
-		int r=2, o=3;
+
+		int r = 2, o = 3;
 		System.out.println(r);
-		
-	System.out.println(o);
-		
+		System.out.println(o);
+
 	}
 
 }
---
+++ java/eclipse/Edunova02/src/edunova/E02PrimitivniTipovi.java
@@ -0,0 +1,27 @@
+package edunova;
+
+public class E02PrimitivniTipovi {
+	public static void main(String[] args) {
+		
+		boolean aktivan=true;
+		byte maliBroj=-5;
+		short maloVeciBroj=2564;
+		int cijeliBroj=654;
+		long velikiBroj=4536864;
+		float decimalniManji=5.1f;
+		double velikiMaliDecimalniBroj=0.0008;
+		char znak= 'Z';
+		
+		System.out.println(aktivan);
+		System.out.println(maliBroj);
+		System.out.println(maloVeciBroj);
+		System.out.println(cijeliBroj);
+		System.out.println(velikiBroj);
+		System.out.println(decimalniManji);
+		System.out.println(velikiMaliDecimalniBroj);
+		System.out.println(znak);
+		
+		
+	}
+
+}
