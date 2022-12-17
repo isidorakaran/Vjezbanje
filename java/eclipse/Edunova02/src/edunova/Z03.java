0689bb52b2590f4c8eeb262c3c29eda4ee8cf699
isidorakaran <karanisidora@hotmail.com>
2022-12-17 13:44:20 +0100

    Zadatak03

---
+++ java/eclipse/Edunova02/src/edunova/Z03.java
@@ -0,0 +1,15 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Z03 {
+	
+	public static void main(String[] args) {
+		int prviBroj=Integer.parseInt(JOptionPane.showInputDialog("Unesi prvi broj"));
+		int drugiBroj=Integer.parseInt(JOptionPane.showInputDialog("Unesi drugi broj"));
+		int rezultat=prviBroj/drugiBroj;
+		System.out.println("Rezultat je = "+rezultat);
+		
+	}
+
+}
