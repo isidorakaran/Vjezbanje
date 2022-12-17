2f79543a5d4479ace876c03e9c65c9995e8bbcbe
isidorakaran <karanisidora@hotmail.com>
2022-12-17 13:16:10 +0100

    Vjezba4

---
+++ java/eclipse/Edunova02/src/edunova/E04PrviProgram.java
@@ -0,0 +1,14 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class E04PrviProgram {
+	
+	public static void main(String[] args) {
+		int prviBroj=Integer.parseInt(JOptionPane.showInputDialog("Unesi prvi broj"));
+		int drugiBroj=Integer.parseInt(JOptionPane.showInputDialog("Unesi drugi broj"));
+		int rezultat=prviBroj+drugiBroj;
+		System.out.println(rezultat);
+	}
+
+}
