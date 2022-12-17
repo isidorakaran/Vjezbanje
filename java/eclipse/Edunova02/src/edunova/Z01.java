8bb21918834c5cc3ab5d5a08e401f1cfb7834dd1
isidorakaran <karanisidora@hotmail.com>
2022-12-17 13:32:57 +0100

    Zadatak01

---
+++ java/eclipse/Edunova02/src/edunova/Z01.java
@@ -0,0 +1,17 @@
+package edunova;
+
+public class Z01 {
+	
+	public static void main(String[] args) {
+		int i=156;
+		char c='X';
+		float f=14.14f;
+		long l=123456789;
+		
+		System.out.println(i);
+		System.out.println(c);
+		System.out.println(f);
+		System.out.println(l);
+	}
+
+}
