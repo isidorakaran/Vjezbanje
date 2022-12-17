ff344604f141d33a36487877bc3f667bc1aaa28e
isidorakaran <karanisidora@hotmail.com>
2022-12-17 13:22:32 +0100

    Vjezba5Operatori

---
+++ java/eclipse/Edunova02/src/edunova/E05Operatori.java
@@ -0,0 +1,15 @@
+package edunova;
+
+public class E05Operatori {
+	public static void main(String[] args) {
+		int a=10,b=3;
+		float r=a/(float)b;
+		System.out.println(r);
+		
+		int i=12%5;
+		System.out.println(i);
+		
+		System.out.println("Isidora"+" "+"Karan");
+	}
+
+}
