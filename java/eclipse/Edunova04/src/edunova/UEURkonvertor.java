2b17ff55d0acc723b0c41402724bb2350052df08
isidorakaran <karanisidora@hotmail.com>
2023-01-11 11:01:09 +0100

    Vjeyba

---
+++ java/eclipse/Edunova04/src/edunova/UEURkonvertor.java
@@ -0,0 +1,27 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class UEURkonvertor {
+	public static void main(String[] args) {
+		String vrsta = JOptionPane.showInputDialog(
+						"1. EUR->KN, 2. KN->EUR");
+		
+		double tecaj = 7.5345;
+		
+		double iznos = Double.parseDouble(
+				JOptionPane.showInputDialog("Iznos u " + 
+				vrsta=="1" ? " EUR" : " KN")
+				);
+		
+		if(vrsta=="1") {
+			System.out.println(iznos*tecaj + " KN");
+		}else {
+			System.out.println(iznos/tecaj + " EUR");
+		}
+		
+		
+	}
+	}
+
+
