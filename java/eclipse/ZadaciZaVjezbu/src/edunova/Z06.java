fa5c126b20701e9be13da8554b0b65b37d27d0ed
isidorakaran <karanisidora@hotmail.com>
2023-01-14 18:31:23 +0100

    Vjezba

---
+++ java/eclipse/ZadaciZaVjezbu/src/edunova/Z06.java
@@ -0,0 +1,20 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Z06 {
+	//saberi sve cifre unesenog prirodnog broja
+	public static void main(String[] args) {
+		int x=Integer.parseInt(JOptionPane.showInputDialog("Unesi prirodan broj:"));
+		int broj = x;
+		int suma=0;
+		while (broj != 0) {
+			suma+=broj%10;
+			broj/=10;
+		}System.out.println("Suma: "+suma);
+	}
+
+
+	
+
+}
