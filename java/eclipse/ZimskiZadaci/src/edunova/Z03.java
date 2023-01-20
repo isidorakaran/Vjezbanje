89f282af165ee9383043693fd3ab99f96c4b3eb5
isidorakaran <karanisidora@hotmail.com>
2023-01-20 13:52:09 +0100

    Vjezba

---
+++ java/eclipse/ZimskiZadaci/src/edunova/Z03.java
@@ -0,0 +1,16 @@
+package edunova;
+
+public class Z03 {
+	//tablica množenja 15x15
+public static void main(String[] args) {
+	for(int i=1;i<=15;i++) {
+		for(int j=1;j<=15;j++) {
+			System.out.printf("%3d ",i*j);
+		}
+		System.out.println();
+	}
+
+	
+	
+}
+}
