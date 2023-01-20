522a491a336fe4e6fdf6913010d95122d9ec42cd
isidorakaran <karanisidora@hotmail.com>
2023-01-20 17:31:35 +0100

    Vjezba

---
+++ java/eclipse/ZadaciZaVjezbu/src/edunova/E08.java
@@ -0,0 +1,17 @@
+package edunova;
+
+public class E08 {
+	//Potražite pa ispišite brojeve djeljive sa 7 iz raspona od 200 do 300
+	//ispis neka bude oblika:
+	//Brojevi djeljivi sa 7 iz raspona 200 do 300 su:
+	//-----
+	public static void main(String[] args) {
+		System.out.println("Brojevi djeljivi sa 7 iz raspona 200 do 300 su:");
+		for(int i=200;i<=300;i++) {
+			if(i%7==0) {
+				System.out.print(i+" ");
+			}
+		}
+	}
+
+}
