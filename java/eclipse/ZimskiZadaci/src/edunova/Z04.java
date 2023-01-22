2ab762defc10af3f2dc1b9bae83f30870feea433
isidorakaran <karanisidora@hotmail.com>
2023-01-22 13:54:55 +0100

    Vjezba

--- java/eclipse/ZimskiZadaci/src/edunova/Z04.java
+++ java/eclipse/ZimskiZadaci/src/edunova/Z04.java
@@ -7,12 +7,25 @@ public class Z04 {
 
 	public static void main(String[] args) {
 		int[][] x = new int[4][4];
+		int[][] y=new int[4][4];
 
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
-				x[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Unesi brojeve"));
-				System.out.print(x[i][j]);
+				x[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Unesi 16 brojeva za x"));
+			
 			}
+		}
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				y[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Unesi 16 brojeva za y"));
+			}
+			
+		}
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+			System.out.print(x[i][j]+y[i][j]+" ");
+			}
+			System.out.println();
 		}
 	}
 
