979b584de4dce39448473b10626121d55a1facad
isidorakaran <karanisidora@hotmail.com>
2023-01-22 14:55:03 +0100

    Vjezba

---
+++ java/eclipse/ZadaciZaVjezbu/src/edunova/Z11.java
@@ -0,0 +1,31 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Z11 {
+	//Program računa srednju ocjenu uspjeha učenika. Nakon posljednje ocjene treba unijeti 0. U slučaju da je bilo koja od ocjena 1, ispisuje se poruka:
+	//Negativan uspjeh. Ako se unese vrijednost koja nije iz raspona od 1 do 5, ispisat će se poruka: Pogrešan unos.
+	//Ispis neka bude oblika: Srednja ocjena uspeja je...
+	public static void main(String[] args) {
+		float srednjaOcjena=0;
+		float zbrojOcjena=0;
+		int ocjena;
+		int brojac=0;
+		JOptionPane.showMessageDialog(null,"Nakon posljednje ocjene treba unijeti 0.");
+		do {
+			ocjena=Integer.parseInt(JOptionPane.showInputDialog("Unesi ocjenu"));
+			if(ocjena==1) {
+				JOptionPane.showMessageDialog(null, "Negativan uspjeh");
+			}
+			if(ocjena<1 && ocjena>5) {
+				JOptionPane.showMessageDialog(null,"Pogrešan unos.");
+			}
+			zbrojOcjena+=ocjena;
+			brojac++;
+		}while(ocjena!=0);
+		brojac-=1;
+		srednjaOcjena=zbrojOcjena/brojac;
+		System.out.println("Srednja ocjena uspjeha je "+srednjaOcjena);
+	}
+
+}
