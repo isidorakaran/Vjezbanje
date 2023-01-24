e23a1caab14c00046bf1f716206f2fcf788f3478
isidorakaran <karanisidora@hotmail.com>
2023-01-24 22:50:10 +0100

    Vjezba

--- java/eclipse/Edunova07/src/edunova/Z02.java
+++ java/eclipse/Edunova07/src/edunova/Z02.java
@@ -1,5 +1,15 @@
 package edunova;
 
 public class Z02 {
+	// Program unosi imena dvije osobe
+	// i ispisujue koliko se vole
+	// metodom slučajnog broja od 10 do 90
+	public static void main(String[] args) {
+		String ime1 = Pomocno.obavezanUnosStringa("Unesi svoje ime");
+		String ime2 = Pomocno.obavezanUnosStringa("Unesi ime simpatije");
 
+		int slucajniBroj = Pomocno.slucajniBrojUrasponu(10, 90);
+
+		System.out.println(ime1 + " i " + ime2 + " se vole " + slucajniBroj + "%");
+	}
 }
