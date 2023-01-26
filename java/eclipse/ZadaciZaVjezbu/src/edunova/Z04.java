0c78ab465efa07e8e2d181eaf65f4762081214e4
isidorakaran <karanisidora@hotmail.com>
2023-01-26 22:05:23 +0100

    Uređivanje

--- java/eclipse/ZadaciZaVjezbu/src/edunova/Z04.java
+++ java/eclipse/ZadaciZaVjezbu/src/edunova/Z04.java
@@ -3,26 +3,27 @@ package edunova;
 import javax.swing.JOptionPane;
 
 public class Z04 {
-	/* Napravi program koji omogućava izračun prosječne visine (cm) učenika nekog razreda.
-	 * Program ispisuje koliko ima učenika viših od 180 cm.
+	/*
+	 * Napravi program koji omogućava izračun prosječne visine (cm) učenika nekog
+	 * razreda. Program ispisuje koliko ima učenika viših od 180 cm.
 	 */
 	public static void main(String[] args) {
-		
-		int n= Integer.parseInt(JOptionPane.showInputDialog("Unesi broj učenika"));
+
+		int n = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj učenika"));
 		float visina;
-		float suma=0;
-		int brojac=0;
-		for (int i=1;i<=n;i++) {
-			visina=Float.parseFloat(JOptionPane.showInputDialog("Upiši visinu u cm"));
-			suma+=visina;
-			if(visina>180) {
+		float suma = 0;
+		int brojac = 0;
+		for (int i = 1; i <= n; i++) {
+			visina = Float.parseFloat(JOptionPane.showInputDialog("Upiši visinu u cm"));
+			suma += visina;
+			if (visina > 180) {
 				brojac++;
 			}
-		} 
-		float prosjek=suma/n;
-		System.out.println("Broj učenika viših od 180 je "+brojac);
-		System.out.println("Prosečna visina učenika je "+prosjek+" cm");
-		
+		}
+		float prosjek = suma / n;
+		System.out.println("Broj učenika viših od 180 je " + brojac);
+		System.out.println("Prosečna visina učenika je " + prosjek + " cm");
+
 	}
 
 }
--- java/eclipse/ZadaciZaVjezbu/src/edunova/Z06.java
+++ java/eclipse/ZadaciZaVjezbu/src/edunova/Z06.java
@@ -3,18 +3,16 @@ package edunova;
 import javax.swing.JOptionPane;
 
 public class Z06 {
-	//saberi sve cifre unesenog prirodnog broja
+	// saberi sve cifre unesenog prirodnog broja
 	public static void main(String[] args) {
-		int x=Integer.parseInt(JOptionPane.showInputDialog("Unesi prirodan broj:"));
+		int x = Integer.parseInt(JOptionPane.showInputDialog("Unesi prirodan broj:"));
 		int broj = x;
-		int suma=0;
+		int suma = 0;
 		while (broj != 0) {
-			suma+=broj%10;
-			broj/=10;
-		}System.out.println("Suma: "+suma);
+			suma += broj % 10;
+			broj /= 10;
+		}
+		System.out.println("Suma: " + suma);
 	}
-
-
-	
 
 }
