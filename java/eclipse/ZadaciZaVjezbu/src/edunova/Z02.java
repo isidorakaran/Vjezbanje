e4558c97e001a51e8f2d11813112467795c31ea7
isidorakaran <karanisidora@hotmail.com>
2023-01-26 22:04:58 +0100

    Uređivanje

--- java/eclipse/ZadaciZaVjezbu/src/edunova/Z02.java
+++ java/eclipse/ZadaciZaVjezbu/src/edunova/Z02.java
@@ -3,22 +3,20 @@ package edunova;
 import javax.swing.JOptionPane;
 
 public class Z02 {
-	
-	//Zbrojite N članova niza:
-	//(1 +1/2+1/3+1/4+1/5+...+1/n). Ispis neka bude oblika:
-	//Upiši željeni broj članova niza:
-	//Zbroj...članova niz iznosi...
+
+	// Zbrojite N članova niza:
+	// (1 +1/2+1/3+1/4+1/5+...+1/n). Ispis neka bude oblika:
+	// Upiši željeni broj članova niza:
+	// Zbroj...članova niz iznosi...
 	public static void main(String[] args) {
-		
-	int n=Integer.parseInt(JOptionPane.showInputDialog("Upiši željeni broj članova niza:"));
-	double suma=0;
-	for(int i=1;i<=n;i++) {
-	
-			suma+=(double)1/i;
+
+		int n = Integer.parseInt(JOptionPane.showInputDialog("Upiši željeni broj članova niza:"));
+		double suma = 0;
+		for (int i = 1; i <= n; i++) {
+
+			suma += (double) 1 / i;
 		}
-	
-	System.out.println("Zbroj " + n + " članova niza iznosi " + suma);
+
+		System.out.println("Zbroj " + n + " članova niza iznosi " + suma);
 	}
 }
-
-
--- java/eclipse/ZadaciZaVjezbu/src/edunova/Z03.java
+++ java/eclipse/ZadaciZaVjezbu/src/edunova/Z03.java
@@ -1,15 +1,16 @@
 package edunova;
 
 public class Z03 {
-	//Prebrojite sve prirodne brojeve deljive s 3 iz raspona od 1 do 9999.Ispis neka bude oblika:
-	//U intervalu od 1 do 9999 ima ... brojeva deljivih sa 3
-	
+	// Prebrojite sve prirodne brojeve deljive s 3 iz raspona od 1 do 9999.Ispis
+	// neka bude oblika:
+	// U intervalu od 1 do 9999 ima ... brojeva deljivih sa 3
+
 	public static void main(String[] args) {
-		int brojac=0;
-		for(int i=1;i<=9999;i++) {
-			if(i%3==0) {
+		int brojac = 0;
+		for (int i = 1; i <= 9999; i++) {
+			if (i % 3 == 0) {
 				brojac++;
-				
+
 			}
 		}
 		System.out.println("U intervalu od 1 do 9999 ima " + brojac + " brojeva deljivih sa 3");
