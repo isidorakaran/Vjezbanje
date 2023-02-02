b397203933548b77cbca7364348f1cab1fba6409
isidorakaran <karanisidora@hotmail.com>
2023-02-02 19:18:06 +0100

    Vjezba

---
+++ java/eclipse/ZadaciZaVjezbu/src/edunova/E24.java
@@ -0,0 +1,22 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class E24 {
+	//Potrebno je ispisati prvih N neparnih brojeva. Ispis neka bude oblika:
+public static void main(String[] args) {
+	int i;
+	int n=Integer.parseInt(JOptionPane.showInputDialog("Upiši broj"));
+	System.out.println("Prvih " + n + " neparnih brojeva su: ");
+	for(i=0;i<=n;i++) {
+		if(i%2==1) {
+			if(i!=n) {
+				System.out.print(i + ", ");
+			}else {
+				System.out.print(i);
+			}
+			
+	}				
+
+}}
+}
