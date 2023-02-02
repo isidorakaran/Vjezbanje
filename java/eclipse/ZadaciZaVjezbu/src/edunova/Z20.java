e051f9de03c8e64fe7826572fb03c781401a1bcf
isidorakaran <karanisidora@hotmail.com>
2023-02-02 17:18:46 +0100

    Vjezba

---
+++ java/eclipse/ZadaciZaVjezbu/src/edunova/Z20.java
@@ -0,0 +1,21 @@
+package edunova;
+
+public class Z20 {
+// Potrebno je ispisati sve troznamenkaste brojeve čiji je zbroj znamenaka 5,
+// a zadnja im je znamenka 0 (znamenka jedinice). Ispis neka bude oblika:
+//Troznamenkasti brojevi ciji je zbroj znamenaka 5, a zadnja im je znamenka 0 su:
+	public static void main(String[] args) {
+		int j,d,s,zbroj;
+		for (int i = 100; i <= 999; i++) {
+			j=i%10;
+			d=(i/10)%10;
+			s=i/100;
+			zbroj=j+d+s;
+			if((zbroj==5)&&(j==0)) {
+				System.out.println(i);
+			}
+			
+		}
+	}
+
+}
