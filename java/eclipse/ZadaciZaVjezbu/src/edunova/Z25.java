28fbe06b6f6b7a66f8be952eb7d3afca27ef4683
isidorakaran <karanisidora@hotmail.com>
2023-02-02 19:25:03 +0100

    Vjezba

---
+++ java/eclipse/ZadaciZaVjezbu/src/edunova/Z25.java
@@ -0,0 +1,12 @@
+package edunova;
+
+public class Z25 {
+	//Potrebno je zbrojiti prvih 100 prirodnih brojeva. Ispis neka bude oblika:
+public static void main(String[] args) {
+	int zbroj=0;
+	for(int i=1;i<=100;i++) {
+		zbroj+=i;
+	}System.out.println(zbroj);
+}
+
+}
