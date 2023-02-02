61c80e48dc9a81fdf1386f266d4e76002b165ec3
isidorakaran <karanisidora@hotmail.com>
2023-02-02 17:26:34 +0100

    Vjezba

--- java/eclipse/ZadaciZaVjezbu/src/edunova/Z20.java
+++ java/eclipse/ZadaciZaVjezbu/src/edunova/Z20.java
@@ -5,16 +5,16 @@ public class Z20 {
 // a zadnja im je znamenka 0 (znamenka jedinice). Ispis neka bude oblika:
 //Troznamenkasti brojevi ciji je zbroj znamenaka 5, a zadnja im je znamenka 0 su:
 	public static void main(String[] args) {
-		int j,d,s,zbroj;
+		int j, d, s, zbroj;
 		for (int i = 100; i <= 999; i++) {
-			j=i%10;
-			d=(i/10)%10;
-			s=i/100;
-			zbroj=j+d+s;
-			if((zbroj==5)&&(j==0)) {
+			j = i % 10;
+			d = (i / 10) % 10;
+			s = i / 100;
+			zbroj = j + d + s;
+			if ((zbroj == 5) && (j == 0)) {
 				System.out.println(i);
 			}
-			
+
 		}
 	}
 
---
+++ java/eclipse/ZadaciZaVjezbu/src/edunova/Z21.java
@@ -0,0 +1,14 @@
+package edunova;
+
+public class Z21 {
+	//tablica mnozenja
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=10;i++) {
+			for(int j=1;j<=10;j++) {
+				System.out.printf("%3d",i*j);
+			}System.out.println();
+		}
+	}
+	
+}
