2ddd02779b0ff61902b84cacfa45404bce49d403
isidorakaran <karanisidora@hotmail.com>
2023-02-03 17:26:48 +0100

    Vjezba

--- java/eclipse/ZadaciZaVjezbu/src/edunova/Z32.java
+++ java/eclipse/ZadaciZaVjezbu/src/edunova/Z32.java
@@ -1,5 +1,23 @@
 package edunova;
 
-public class Z32 {
+import javax.swing.JOptionPane;
 
+public class Z32 {
+//Potrebno je provjeriti da li je odabrani prirodni broj savršen
+//(broj je savršen ako je jednak zbroju svih svojih djelitelja, osim njega samog).
+	public static void main(String[] args) {
+		int zbroj = 0;
+		int savrsenBr = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj"));
+		for (int i = 1; i <= (savrsenBr - 1); i++) {
+			if (savrsenBr % i == 0) {
+				zbroj += i;
+			}
+		}
+		if (zbroj == savrsenBr) {
+			System.out.println("Broj je savršen");
+		} else {
+			System.out.println("Broj nije savršen");
+		}
+
+	}
 }
