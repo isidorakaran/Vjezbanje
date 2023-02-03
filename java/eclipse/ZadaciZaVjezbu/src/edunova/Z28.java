3ef7111ce314bd451a04020e6acee0c1176d6e8d
isidorakaran <karanisidora@hotmail.com>
2023-02-03 16:33:03 +0100

    Vjezba

---
+++ java/eclipse/ZadaciZaVjezbu/src/edunova/Z28.java
@@ -0,0 +1,12 @@
+package edunova;
+
+public class Z28 {
+	//Potrebno je zbrojiti sve prirodne troznamenkaste brojeve.
+	public static void main(String[] args) {
+		int zbroj=0;
+		for(int i=100;i<=999;i++) {
+			zbroj+=i;
+		}System.out.println(zbroj);
+	}
+
+}
